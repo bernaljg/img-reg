bc8d60e30f4fc2b2af17da863e4efba874bfa0db
Bernal Jimenez <bernaljg@berkeley.edu>
2016-03-17 17:49:55 -0700

    Added timing functionality

--- postSelectMoviesNMJ_V6.m
+++ postSelectMoviesNMJ_V6.m
@@ -11,7 +11,6 @@ nFrames = 2000;
 
 for movieNum=1:nMovies;
 
-    tic
 
     % Loads variables
     cd(mov_to_reg_dir)
@@ -22,10 +21,12 @@ for movieNum=1:nMovies;
     disp(['Loading reader for NMJ #: ',num2str(nmjNum)]);   
     reader = bfGetReader(file_name);
     disp(['Finished loading reader for NMJ #: ',num2str(nmjNum)]);   
-
+    
+    tic
     % Calculates tracking coordinates for all nmjs 
     trackingCoordinates = find_tracking_coor(reader,regPropsStore,maxFrame,maxFrameNum,nFrames,NMJs)
-    
+    timetotrack = toc
+
     % Makes output folder
     cd(output_dir)
     FileNameApp = FileName;
@@ -33,15 +34,12 @@ for movieNum=1:nMovies;
     mkdir(FileNameApp);
     cd(FileNameApp);
     copyfile([mov_to_reg_dir '/' roiFiles(movieNum).name],cd)
-        
+    
+    tic
     % Saves smoothed movies for all nmjs in seperate folders
     save_smooth_coors(reader,trackingCoordinates,nFrames,maxFrameNum,FileNameApp)
-
-    toc   
-
+    timetosavetrack = toc
 end
-
-tic
 
 
 %%% Applies Affine Transformations on NMJs for all Movies
@@ -62,13 +60,18 @@ for movieNum=1:nMovies;
     % Loads all tracked nmjs for this movie into array
     tracked_nmjs = load_tracked_nmjs(nNmjs,trackedFileNames)
     
+    tic
     % Finds affine transform for all nmjs in this movie
     affineTransforms = find_affine_transf(roiFiles,movieNum,tracked_nmjs)
+    timeaffine = toc
 
+    tic
     % Applies affine transformation and saves movies for all nmjs in this movie 
     save_affine_mov(affineTransforms,tracked_nmjs,trackedFileNames,maxFrameNum,nFrames,nNmjs)
+    timesaveaffine = toc
+
 end
-toc
+
 
 %%% Applies Demon Transformations on NMJs for all Movies
 
@@ -88,13 +91,16 @@ for movieNum=1:nMovies;
     % Loads affined nmj movies into array
     affined_nmjs = load_affined_nmjs(nNmjs,affinedFileNames)
 
+    tic
     % Finds and applies demon transformation onto affined nmjs in this movie
     [demonized_mov, disp_fields] = apply_demon_transf(roiFiles,movieNum,affined_nmjs)
-
+    timedemon = toc
+    
+    tic
     % Saves demonized nmj movies for this movie
     save_demon_mov(demonized_mov,disp_fields,affinedFileNames,nNmjs)
-    
-toc
+    timesavedemon = toc
+
 cd(mov_to_reg_dir)
 
 end
