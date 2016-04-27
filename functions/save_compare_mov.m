2c6cb245e141b9c78ed9728599d3e58f2efa34b0
Bernal Jimenez <bernaljg@ln000.brc>
2016-04-26 21:16:00 -0700

    Compare movie saving minor changes

--- functions/save_compare_mov.m
+++ functions/save_compare_mov.m
@@ -1,15 +1,11 @@
 %Created by: Bernal Jimenez
 %4/11/2016
 
-function [] = save_compare_mov(directory, filename)
+function [] = save_compare_mov(directory, movie1, movie2)
 
-load([directory '/demon/' filename],'demon')
-load([directory '/demon_affine/' filename],'demon_affine')
 size_mov = size(demon);
 padding = zeros(25,size_mov(2),size_mov(3));
 
-movie1 = demon;
-movie2 = demon_affine;
 compare_mov = cat(1,movie1,padding,movie2);
 
-save_movie(compare_mov, fullfile(directory, [filename '_Comparison_Movie']))
+save_movie(compare_mov, fullfile(directory, 'Comparison_Movie2'))
